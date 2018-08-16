f2114b1b00ca0eeac4fcd681e7f7b850090ffa8a
Tristan Chastain <TristanChastain@Gmail.com>
2018-08-16 08:42:52 -0400

    Test

--- DAL/Objects/userDAO.cs
+++ DAL/Objects/userDAO.cs
@@ -11,6 +11,6 @@ namespace DAL.Objects
         public int User_ID { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public int Role_ID { get; set; }
+       // public int Role_ID { get; set; }
    }
 }
