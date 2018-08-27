1fafb746edc4bdaaa0d944ff74d6856e36fc01f0
Tristan Chastain <TristanChastain@Gmail.com>
2018-08-27 14:23:53 -0400

    push test

--- DAL/Objects/albumDAO.cs
+++ DAL/Objects/albumDAO.cs
@@ -8,7 +8,7 @@ namespace DAL.Objects
 {
    public class albumDAO
         //comment for the push
-   {    //comment for another push
+   {    //comment
         public int Albums_ID { get; set; }
         public string AlbumName { get; set; }
         public string AlbumDescription { get; set; }
